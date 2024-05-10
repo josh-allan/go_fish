502d9e40d75bfb01acab14fb4b378c99150b7225
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-05-10 17:29:56 +1000

    parser function call update

--- go_fish/parser/parser.go
+++ go_fish/parser/parser.go
@@ -5,40 +5,17 @@ import (
 	"github.com/mmcdole/gofeed"
 )
 
-type MyCustomTranslator struct {
-	defaultTranslator *gofeed.DefaultRSSTranslator
-}
-
-func NewMyCustomTranslator() *MyCustomTranslator {
-	t := &MyCustomTranslator{}
-	t.defaultTranslator = &gofeed.DefaultRSSTranslator{}
-	return t
-}
-
-/*
-func (ct *MyCustomTranslator) Translate(feed interface{}) (*gofeed.Feed, error) {
-	rss, found := feed.(*rss.Feed)
-	if !found {
-		return nil, fmt.Errorf("Feed did not match expected type of *rss.Feed")
-	}
-
-	f, err := ct.defaultTranslator.Translate(rss)
+func feed() {
+	url := "https://ozbargain.com.au/feed"
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	if rss.ITunesExt != nil && rss.ITunesExt.Author != "" {
-		f.Author = rss.ITunesExt.Author
-	} else {
-		f.Author = rss.ManagingEditor
-	}
-	return f, nil
+	// for entry
+	fmt.Println(feed.PublishedParsed)
 }
-*/
 
 func main() {
-	feedData := "https://ozbargain.com.au/feed"
-	fp := gofeed.NewParser()
-	fp.RSSTranslator = NewMyCustomTranslator()
-	feed, _ := fp.ParseString(feedData)
-	fmt.Println(feed.Items)
+	feed()
 }
