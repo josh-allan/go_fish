5351c0e0f9f967b216aae6dd0fb2782d918a4f5a
Josh Allan <joshua.allan92@gmail.com>
2024-05-13 07:53:33 +1000

    parser feed search and main loop

--- go_fish/main.go
+++ go_fish/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	//"fmt"
 	"fmt"
+	"time"
+
 	"github.com/joho/godotenv"
 	//"github.com/josh-allan/terraforming-mars/discord"
-	"github.com/josh-allan/terraforming-mars/parser"
 	"log"
+
+	"github.com/josh-allan/terraforming-mars/parser"
 	//"os"
 )
 
@@ -17,6 +21,31 @@ func main() {
 
 	// webhook_URL := os.Getenv("DISCORD_WEBHOOK_URL")
 	// fmt.Println(webhook_URL)
-	feed := parser.Feed()
-	fmt.Println(feed)
+	interestingSearches := []string{"Nvidia"}
+	feedUrl := "https://ozbargain.com.au/feed"
+	var lastUpdated *time.Time
+	matchedIDs := make(map[string]bool)
+	parser.Feed(feedUrl, interestingSearches, lastUpdated, matchedIDs)
+	for {
+		matchingEntries, lastUpdated, newMatchedIDs, err := parser.Feed(feedUrl, interestingSearches, lastUpdated, matchedIDs)
+		if err != nil {
+			log.Printf("Error searching feed: %v\n", err)
+			continue
+		}
+
+		if len(matchingEntries) > 0 {
+			for _, entry := range matchingEntries {
+				fmt.Printf("Matching entry found in %s: %s at %s\n", feedUrl, entry.Title, time.Now().Format("02/01/2006, 15:04:05"))
+			}
+		} else {
+			fmt.Printf("No new matching entries found in %s.\n", feedUrl)
+		}
+
+		// Add the new matched IDs to the matched IDs map
+		for _, id := range newMatchedIDs {
+			matchedIDs[id] = true
+		}
+
+		time.Sleep(60 * time.Second) // Wait 60 seconds for the next run
+	}
 }
--- go_fish/parser/parser.go
+++ go_fish/parser/parser.go
@@ -1,18 +1,49 @@
 package parser
 
 import (
-	"fmt"
 	"github.com/mmcdole/gofeed"
+	"strings"
+	"time"
 )
 
-func Feed() {
-	url := "https://ozbargain.com.au/feed"
+func ContainsIngoreCase(s, substr string) bool {
+	s, substr = strings.ToUpper(s), strings.ToUpper(substr)
+	return strings.Contains(s, substr)
+}
+
+func Feed(feedUrl string, intestingSearches []string, lastUpdated *time.Time, matchedIDs map[string]bool) ([]*gofeed.Item, *time.Time, []string, error) {
 	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL(url)
+	feed, err := fp.ParseURL(feedUrl)
 	if err != nil {
 		panic(err)
 	}
-	// for entry
-	return feed
+	var matchingEntries []*gofeed.Item
+	var newMatchedIDs []string
 
+	for _, entry := range feed.Items {
+		publishedTime := entry.PublishedParsed
+		// Skip over entries that were published before the last entry that was processed
+		if lastUpdated != nil && publishedTime != nil && publishedTime.Before(*lastUpdated) {
+			continue
+		}
+
+		// We also want to skip over entries that have already been matched
+		if matchedIDs != nil && matchedIDs[entry.GUID] {
+			continue
+		}
+
+		// If we match on a new string, let's append it to the appropriate list
+		// ensuring to only alert on a new entry
+		for _, term := range interestingSearches {
+			if term != "" && (ContainsIgnoreCase(entry.Title, term) || ContainsIgnoreCase(entry.Description, term)) {
+				matchingEntries = append(matchingEntries, entry)
+				newMatchedIDs = append(newMatchedIDs, entry.GUID)
+				break
+			}
+		}
+	}
+
+	// Return the matching entries, the published time of the last entry,
+	// and the IDs of the new matching entries
+	return matchingEntries, feed.Items[len(feed.Items)-1].PublishedParsed, newMatchedIDs, nil
 }
