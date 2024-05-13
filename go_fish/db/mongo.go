d8e398ae04105595869a0c7cedcb9562638f3aa7
Josh Allan <46344627+josh-allan@users.noreply.github.com>
2024-05-14 08:04:36 +1000

    env loader function

--- go_fish/db/mongo.go
+++ go_fish/db/mongo.go
@@ -3,15 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
+func loadDotEnv() {
+	godotenv.Load("../.env")
+	return
+}
+
+func ConnectToMongoDB() {
+	loadDotEnv()
+	atlas_uri := os.Getenv("ATLAS_URI")
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI().SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI(atlas_uri).SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
