796b7620feb46f51e29d78a1b75bb0c636bcff31
Josh Allan <joshua.allan92@gmail.com>
2024-05-20 07:37:11 +1000

    continue type agnostic refactor

--- go_fish/db/mongo_datastore.go
+++ go_fish/db/mongo_datastore.go
@@ -1,1 +1,11 @@
 package db
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type MatchingDocuments struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Name          string             `bson:"name"`
+	PublishedTime primitive.DateTime `bson:"publishedtime"`
+	Url           string             `bson:"url"`
+	GUID          string             `bson:"guid"`
+}
--- go_fish/util/shared.go
+++ go_fish/util/shared.go
@@ -1,17 +1,5 @@
 package shared
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
-type MatchingDocuments struct {
-	ID            primitive.ObjectID `bson:"_id"`
-	Name          string             `bson:"name"`
-	PublishedTime primitive.DateTime `bson:"publishedtime"`
-	Url           string             `bson:"url"`
-	GUID          string             `bson:"guid"`
-}
-
 var SearchTerms = []string{
 	"Samsung",
 	"Steam",
